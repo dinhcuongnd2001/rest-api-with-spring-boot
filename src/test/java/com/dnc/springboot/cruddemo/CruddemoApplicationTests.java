26039aa4777f106f285a13a0fe09875b59e7dc90
dncuong <dinhcuongnd2001@gmail.com>
2024-05-26 09:42:15 +0700

    initial project

---
+++ src/test/java/com/dnc/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnc.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
